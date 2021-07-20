6cbae6c6e86b683ee2ec5152517525821f738be3
savvas pafitis <savvazp@gmail.com>
2021-07-21 00:41:37 +0100

    added perplexity and fixed confidence to product

--- helpers/language_modelling.py
+++ helpers/language_modelling.py
@@ -1,4 +1,5 @@
 import numpy
+from numpy.core.fromnumeric import prod
 import torch
 from transformers import \
     pipeline, \
@@ -28,6 +29,14 @@ def find_nth_substring(sentence, substring, n):
         n -= 1
     return start
 
+def compute_perplexity(input_list, is_log = True):
+    if is_log:
+        input_list = np.exp(input_list)
+    product = np.prod(1 / input_list)
+    length = len(input_list)
+
+    return product ** (1/length)
+
 def multitoken_prediction(
     sentence_list, model, tokenizer, top_k_vocab = 1, 
     verbose = False, confidence_normalizer = 'token-length'):
@@ -55,9 +64,8 @@ def multitoken_prediction(
         if not num_tokens:
             raise ValueError('No masks found in sentence.')
 
-        # total_confidence = 1
-        prod_confidence = 1
-        sum_confidence = 0
+        sequence_confidence = 0
+        sequence_confidences = []
 
         answer_given = []
 
@@ -75,21 +83,22 @@ def multitoken_prediction(
             # convert to probabilities
             probabilities = torch.softmax(
                 outputs.logits[0, masked_idxs[:, 1]], dim = 1)
+            logprobs = torch.log(probabilities)
 
-            # obtain k most confident token predictions
-            mask_confidence, token_ids = torch.topk(probabilities, top_k_vocab)
+            # obtain k most confident token predictions, work on logprobs to avoid underflow
+            mask_confidence, token_ids = torch.topk(logprobs, top_k_vocab)
 
             # selects the mask index that correspond to the most confident prediction; I am slicing [0] because of top_k_vocab will return the k most confident possible tokens. ultimately top_k_vocab is not used, but I am keeping it here for future work
             most_confident = mask_confidence.argmax(dim = 0)[0].item()
             target_token_idx = token_ids[most_confident][0]
             target_token = tokenizer.decode(target_token_idx)
             
-            # log confidence as a proxy of probability
-            current_confidence = mask_confidence[most_confident][0].item()
+            # confidence as a proxy of probability
+            token_confidence = mask_confidence[most_confident][0].item()
             
-            # I add to sum_confidence (cumulative) the multiplied "probability"/ likelihood as sometimes the next token has conf = 1 and throws off the calculations.
-            prod_confidence *= current_confidence
-            sum_confidence += prod_confidence
+            # add logprobabilities to obtain sequence probability
+            sequence_confidence += token_confidence
+            sequence_confidences.append(token_confidence)
             
             # find start and end index of <mask> to be removed
             starting_pos = find_nth_substring(
@@ -101,32 +110,39 @@ def multitoken_prediction(
             sentence = sentence[:starting_pos] + \
                 target_token + sentence[ending_pos:]
 
+            # answer_given = [ (token, position), ... ]
             answer_given.append((target_token, starting_pos))
-            # answer_given.append((target_token, target_token_idx))
 
             if verbose:
-                print(f'Iteration: {_iter}, Predicted Token: {target_token}, Iteration Confidence: {current_confidence}, Confidence (multiplicative): {prod_confidence}, Confidence (summative): {sum_confidence}')
+                print(f'Iteration: {_iter}, Predicted Token: {target_token}, Iteration Confidence: {token_confidence}, Confidence (sequence): {sequence_confidence}')
                 print(f'Sentence: {sentence}')
 
+        # construct answer in correct order, as we predict tokens in random positions
         final_answer = [x[0] for x in sorted(answer_given, key = lambda x: x[1])]
         # returns list of tokens, combine them together and make sure we remove the starting whitespace
         num_tokens = len(answer_given)
         final_answer = ''.join(final_answer)
         final_answer = final_answer[1:] if final_answer[0] == ' ' else final_answer
+        
+        # compute perplexity
+        # minimizing perplexity is equivalent to maximizing the test set probability according to the language model.
+        perplexity = compute_perplexity(sequence_confidences, is_log=True)
 
         all_answers.append(
-            (final_answer, sum_confidence, prod_confidence, num_tokens))
+            (final_answer, sequence_confidence, perplexity, sequence_confidences, num_tokens))
 
-    if confidence_normalizer == 'token-length':
-        confidences = [
-            _sum_confidence / _num_tokens for \
-                 _, _sum_confidence, _, _num_tokens in all_answers]
-    if confidence_normalizer == 'char-length':
-        confidences = [x[1] / len(x[0]) for x in all_answers]
+    # if confidence_normalizer == 'token-length':
+    #     confidences = [
+    #         _sum_confidence / _num_tokens for \
+    #              _, _sum_confidence, _, _num_tokens in all_answers]
+    # if confidence_normalizer == 'char-length':
+    #     confidences = [x[1] / len(x[0]) for x in all_answers]
     
-    answer, confidence, _, _ = all_answers[np.argmax(confidences)]
+    most_confident = np.argmax([x[1] for x in all_answers])
+    less_perplex = np.argmin([x[2] for x in all_answers])
+    
+    return all_answers[most_confident], all_answers[less_perplex]
 
-    return answer, confidence
 
 def check_model(model, tokenizer, input_sentence):
     '''
@@ -141,7 +157,8 @@ def check_model(model, tokenizer, input_sentence):
         # somewhat hacky
         # checks if sentence contains any of the masks
         # and replaces it with the appropriate tokenizer.mask_token
-        x = [sentence.replace(x, fill_mask.tokenizer.mask_token) for x in masks if x in sentence]
+        x = [sentence.replace(x, fill_mask.tokenizer.mask_token) \
+            for x in masks if x in sentence]
         if len(x):
             return x[0]
 
@@ -194,8 +211,9 @@ def run_language_model(
             # similarly, for single token masks we utilise transformers.pipeline functionality, where for multi_token ones we had to write our own. functionality is largely the same
             # returns the predicted answer with the corresponding confidence
             if multi_token:
-                answer_given, confidence = multitoken_prediction(
-                    cloze.cloze_text, model, tokenizer, top_k_vocab = 1, verbose = False
+                answer_confidence, answer_perplexity = multitoken_prediction(
+                    cloze.cloze_text, model, tokenizer, 
+                    top_k_vocab = 1, verbose = False
                 )
             else:
                 result = check_model(model, tokenizer, cloze.cloze_text)
@@ -208,7 +226,10 @@ def run_language_model(
 
             # saves prediction, confidence score, truth, dataframe row, cloze id
             row_result.append(
-                (answer_given, confidence, answer_true, row, cloze.cloze_id))
+                (answer_confidence, answer_perplexity, answer_true, row, cloze.cloze_id)
+            )
+            # row_result.append(
+            #     (answer_given, confidence, answer_true, row, cloze.cloze_id))
 
         results.append(row_result)
 
@@ -221,7 +242,8 @@ def run_language_model(
     entities = dict()
     entities = {f'{x}':[] for x in categories if x not in entities}
     # append only unique values
-    [entities.get(x[1]).append(x[0]) for x in entity_set if x[0] not in entities.get(x[1])]
+    [entities.get(x[1]).append(x[0]) \
+        for x in entity_set if x[0] not in entities.get(x[1])]
     
     if save_results:
         model_name = model._get_name()
@@ -233,8 +255,6 @@ def run_language_model(
         with open(
             f'results/{model_name}_{current_time}_entity_set.pickle', 'wb') as f:
             pickle.dump(entity_set, f)
-
-
         with open(
             f'results/{model_name}_{current_time}_entity_dictionary.json', 'w') as f:
             json.dump(entities, f)
